bd5c72db600706b858c61216a04f908046606bd3
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-15 21:25:40 -0600

    hola

---
+++ proyect/Login-Register/assets/php/conexion.php
@@ -0,0 +1,15 @@
+<?php
+    $conexion = mysqli_connect("127.0.0.1: 3307
+    ","root","","login-register");
+
+    /* 
+    if($conexion) // Si los parametros de nuestra conexion son correctos
+    {
+        echo 'Conectado exitosamente a la Base de Datos';
+    }
+    else // Si los parametros de nuestra conexion no son correctos
+    {
+        echo 'No se ha podido conectar a la Base de Datos';
+    }
+    */
+?>
