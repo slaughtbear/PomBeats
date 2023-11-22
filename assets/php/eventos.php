3aacf2fc5917196fb121b4af30d524bed61ba83a
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-22 10:15:34 -0600

    Se_creo_el_archivo_de_los_eventos

---
+++ assets/php/eventos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <form action="#">
+            <input type="text"  name="titulo" placeholder="Nombre del evento">
+            <input type="text"  name="lugar" placeholder="">
+            <input type="text"  name="fecha" placeholder="Fecha y hora del evento">
+            <input type="textarea"  name="descripcion" placeholder="Descripcion del evento">
+            <input type="file"  name="portada" placeholder= "select img">
+            <input type="submit" value="Publicar">
+        </form>
+    </div>
+</body>
+</html>
