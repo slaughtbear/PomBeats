2ce0f3dc846332aae468f84733f67026beea7712
slaughtbear <ivanaguirre368@gmail.com>
2023-11-26 09:47:56 -0600

    Renombre el archivo css para usarlo solo para la pagina del login, y modifique la ruta del link en el index

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <meta name="keywords" content="Iniciar, Registrar"> <!-- Metadatos de palabras clave -->
     <!-- L I N K S -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> <!-- Bootstrap -->
-    <link href="css/style.css" rel="stylesheet"> <!-- CSS -->
+    <link href="css/login.css" rel="stylesheet"> <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"> <!-- Iconos de Bootstrap -->
     <link rel="icon" type="image/x-icon" href="../img/logo.png">
     <!-- Icono -->
