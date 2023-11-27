86b1d8bd3d95ff78aa5ee2ef205e05d8efeb7061
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-26 19:00:42 -0600

    Se restablecio todo solo se agrego la validicion de las contrasenas

--- php/registro.php
+++ php/registro.php
@@ -7,24 +7,40 @@
     $correo = $_POST['correo'];
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
-    $contrasena2 =$_post["contrasena2"];
+    $contrasena2 =$_post["contrasena2"]
     $contrasena = hash('sha512', $contrasena); //Encriptamiento de las contraseñas
 
     // Consulta SQL para registrar usuarios
-    $query = "INSERT INTO users (nombre, correo, usuario, contrasena) VALUES ('$nombre', '$correo', '$usuario', '$contrasena,$contrasena2')";
+    $query = "INSERT INTO users (nombre, correo, usuario, contrasena) VALUES ('$nombre', '$correo', '$usuario', 
+    '$contrasena,$contrasena2')";
 
-    //Verificar las contrasenas
-    $verificar_usuario = mysqli_query($conexion,"SELECT * FROM users WHERE contrasena = '$contrasena' and contrasena2 = '$contrasena2'");
-    if if(mysqli_num_rows($verificar_usuario) > 0) {
+//Verificar las contraseñas coincidan 
+$verificar_correo = mysqli_query($conexion,"SELECT * FROM users WHERE contrasena = '$contrasena' 
+and contrasena2 = '$contrasena2' ");
+       
+         if(mysqli_num_rows($Validar_contrasenas) > 0) {
+            echo '
+            <script>
+            alert("Este correo ya está registrado, intenta con otro diferente");
+            window.location = "../index.php";
+         </script>
+        ';
+        exit();
+    }
+
+    // Verificar que el correo no se repita en la base de datos
+    $verificar_correo = mysqli_query($conexion,"SELECT * FROM users WHERE correo = '$correo' ");
+
+    if(mysqli_num_rows($verificar_correo) > 0) {
         echo '
             <script>
-                alert("Las contraseñas no coinciden");
+                alert("Este correo ya está registrado, intenta con otro diferente");
                 window.location = "../index.php";
             </script>
         ';
         exit();
     }
-    
+
     // Verificar que el usuario no se repita en la base de datos
     $verificar_usuario = mysqli_query($conexion,"SELECT * FROM users WHERE usuario = '$usuario' ");
 
