5668723473cce6bdefc248ccd9d78fe77e86a587
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-27 14:53:46 -0600

    prueba de que se suben los archivos

--- php/Uploads.php
+++ php/Uploads.php
@@ -1,7 +1,7 @@
 <?php
     
 
-   ;
+   
     $target_file = $target_dir . basename($_FILES["portada"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
