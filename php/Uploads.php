e3dbd06b7c498bb0c2c79f4faf81d84ada9b4f77
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-26 22:24:25 -0600

    se crea la carpeta de las imagenes

---
+++ php/Uploads.php
@@ -0,0 +1,38 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$archivo = $_FILES["fileToUpload"]["tmp_name"];
+$ruta = "uploads/" . $$target_file;
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+            $img=$_FILES["fileToUpload"]["name"];
+           
+
+            require 'conexion.php';
+
+
+
+            $sql = "INSERT INTO img (fileToUpload)
+            VALUES ('$img')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+                header('Location: Mostrarimg.php');
+                } else { 
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                }          
+
+                    $conn->close();
+        echo "File is an image - " . $check["mime"] . ".";
+    echo $target_file;
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+?>
