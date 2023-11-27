c94e06db474350264b654807f154e75ee5aad17f
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-27 16:04:06 -0600

    prueba de que se suben los archivos

--- php/eventos.php
+++ php/eventos.php
@@ -7,13 +7,13 @@
 </head>
 <body>
     <form action="Uploads.php" method="POST" enctype="multipart/form-data">
-        <input type="text" name="titulo" placeholder = "titulo" required>
-        <input type="text" name ="lugar" placeholder = "lugar" required>
-        <input type="date" name ="fecha" placeholder = "fecha" required>
-        <input type="textarea" name ="descripcion" required>
-        <input type="file" name ="portada" required>
+        <input type="text" name="titulo" placeholder="titulo" required>
+        <input type="text" name="lugar" placeholder="lugar" required>
+        <input type="date" name="fecha" placeholder="fecha" required>
+        <!-- Cambié 'textarea' a 'text' para el campo de descripción -->
+        <input type="text" name="descripcion" placeholder="descripcion" required>
+        <input type="file" name="portada" required>
         <input type="submit" name="enviar">
-
     </form>
 </body>
 </html>
