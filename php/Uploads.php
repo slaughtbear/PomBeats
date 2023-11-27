5ef78559e90c943c603d5009a5ce794f706fd452
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-27 00:23:25 -0600

    prueba de que se suben los archivos

--- php/Uploads.php
+++ php/Uploads.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "img-events";
+$target_dir = "img-events/";
 $target_file = $target_dir . basename($_FILES["portada"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
