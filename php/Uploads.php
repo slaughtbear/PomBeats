e7ce961643ea21b8a214193d6aebe903c0683de4
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-27 16:00:23 -0600

    prueba de que se suben los archivos

--- php/Uploads.php
+++ php/Uploads.php
@@ -3,46 +3,54 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 $target_dir = "../img-events/";
-$target_file = $target_dir . basename($_FILES["portada"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-$portada_tmp = $_FILES["portada"]["tmp_name"];
-$titulo = $_POST["titulo"];
-$lugar = $_POST["lugar"];
-$fecha = $_POST["fecha"];
-$descripcion = $_POST["descripcion"];
-$ruta = $target_dir . $target_file;
 
-// Check if the form was submitted
-if (isset($_POST["submit"])) {
-    // Check if the file is an actual image
-    $check = getimagesize($portada_tmp);
-    if ($check !== false) {
-        // Upload the file
-        move_uploaded_file($portada_tmp, $ruta);
+// Verifica si el formulario ha sido enviado y si el campo "portada" está presente en el array $_FILES
+if (isset($_POST["submit"]) && isset($_FILES["portada"])) {
+    $target_file = $target_dir . basename($_FILES["portada"]["name"]);
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $portada_tmp = $_FILES["portada"]["tmp_name"];
+    $titulo = $_POST["titulo"];
+    $lugar = $_POST["lugar"];
+    $fecha = $_POST["fecha"];
+    $descripcion = $_POST["descripcion"];
+    $ruta = $target_dir . $target_file;
 
-        // Include database connection
-        include 'conexion.php';
+    // Verifica si la clave "portada" existe en el array $_FILES
+    if (array_key_exists("portada", $_FILES)) {
+        // Check if the file is an actual image
+        $check = getimagesize($portada_tmp);
 
-        // Insert data into the database
-        $sql = "INSERT INTO eventos (titulo, lugar, fecha, descripcion, portada)
-                VALUES ('$titulo', '$lugar', '$fecha', '$descripcion', '$target_file')";
+        if ($check !== false) {
+            $portada = $_FILES["portada"]["name"];
+            include 'conexion.php';
 
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-            //header('Location: Mostrarimg.php');
+            move_uploaded_file($portada_tmp, $ruta);
+
+            $sql = "INSERT INTO eventos (titulo, lugar, fecha, descripcion, portada)
+                    VALUES ('$titulo', '$lugar', '$fecha', '$descripcion', '$portada')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+                // header('Location: Mostrarimg.php');
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+            $conn->close();
+
+            echo "File is an image - " . $check["mime"] . ".";
+            echo $target_file;
+            $uploadOk = 1;
         } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            echo "File is not an image.";
+            $uploadOk = 0;
         }
-
-        $conn->close();
-
-        echo "File is an image - " . $check["mime"] . ".";
-        echo $target_file;
-        $uploadOk = 1;
     } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+        echo "La clave 'portada' no está presente en el array \$_FILES.";
     }
+} else {
+    echo "Formulario no enviado o campo 'portada' no presente en el array \$_FILES.";
 }
 ?>
+
