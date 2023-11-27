2191a5c1d4df47c10931dfb489789c3ffe1b6c88
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-27 14:44:11 -0600

    prueba de que se suben los archivos

--- php/Uploads.php
+++ php/Uploads.php
@@ -1,4 +1,45 @@
 <?php
-    echo $_FILES=["poertada"]["name"];
+    
+
+    $titulo=$_POST["titulo"];
+    $lugar=$_POST["lugar"];
+    $fecha=$_POST["fecha"];
+    $descripcion=$_POST["descripcion"];
+    $target_file = $target_dir . basename($_FILES["portada"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $portada = $_FILES["portada"]["tmp_name"];
+    $ruta = "../img-events" . $target_file;
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+      $check = getimagesize($_FILES["portada"]["tmp_name"]);
+      if($check !== false) {
+                $img=$_FILES["portada"]["name"];
+               
+    
+                include 'conexion.php';
+    
+    
+    
+                $sql = "INSERT INTO eventos (titulo,lugar,fecha,descripcion,
+                portada)
+                VALUES ('$titulo','$lugar','$fecha',$descripcion,)";
+    
+                if ($conn->query($sql) === TRUE) {
+                    echo "New record created successfully";
+                    //header('Location: Mostrarimg.php');
+                    } else { 
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                    }          
+    
+                        $conn->close();
+            echo "File is an image - " . $check["mime"] . ".";
+        echo $target_file;
+        $uploadOk = 1;
+      } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+      }
+    }
     
 ?>
