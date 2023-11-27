f6616348982ec415fb5b75bcfd313f489d01a49d
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-26 22:38:42 -0600

    se creo el forulario para subir los eventos

---
+++ php/eventos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Uploads.php">
+        <input type="text" name="titulo" placeholder = "titulo" required>
+        <input type="text" name ="lugar" placeholder = "lugar" required>
+        <input type="date" name ="fecha" placeholder = "fecha" required>
+        <input type="textarea" name ="descripcion" required>
+        <input type="file" name ="portada" required>
+        <input type="submit" name="enviar">
+
+    </form>
+</body>
+</html>
