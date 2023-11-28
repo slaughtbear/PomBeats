f29c1958233d9482fa17d5a141dd29a8d743ca6c
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-28 08:45:55 -0600

    se hicieron los cambios de idiom

--- php/eventos.php
+++ php/eventos.php
@@ -4,7 +4,7 @@ ini_set('display_errors', '1');
 
    include 'conexion.php';
    
-$sql = "SELECT * FROM eventos";
+$sql = "SELECT * FROM eventos where idEventos = $idEventos";
 $result = $conexion->query($sql);
 
 if ($result->num_rows > 0) {
