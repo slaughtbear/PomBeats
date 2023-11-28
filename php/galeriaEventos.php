8ef8e27957a9ec05b54b74e23799db4c1071a510
slaughtbear <ivanaguirre368@gmail.com>
2023-11-28 12:05:47 -0600

    Cambie la direccion del modal para registrar eventos

--- php/galeriaEventos.php
+++ php/galeriaEventos.php
@@ -93,7 +93,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form action="#" method="POST">
+        <form action="guardarEventos.php" method="POST">
           <div class="mb-4">
               <label for="titulo" class="form-label text-white">Título</label>
               <input type="text" class="form-control custom-input" name="titulo" id="titulo" required>
