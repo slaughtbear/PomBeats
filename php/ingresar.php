4fae3a71c82e6a66201a9005521444f2bc71f7cb
slaughtbear <ivanaguirre368@gmail.com>
2023-11-28 16:42:58 -0600

    otra prueba

--- php/ingresar.php
+++ php/ingresar.php
@@ -13,6 +13,11 @@ session_start(); // Inicia sesión
 
     // Validacion de usuario
     $validar_login = mysqli_query($conexion, "SELECT * FROM users WHERE correo = '$correo' and contrasena = '$contrasena'");
+
+    // Verificar rol
+    $verificar_rol = mysqli_query($conexion, "SELECT * FROM users WHERE rol = 1 LIMIT 1");
+
+
     if ($contrasena!=$contrasena2) {
         echo '
         <script>
@@ -20,6 +25,8 @@ session_start(); // Inicia sesión
             window.location = "../index.php";
         </script>
             ';
+    } else if ($rol == 1) {
+        header("location: https://www.youtube.com/watch?v=AWjCxj9D_Ko&t=32s");
     } elseif(mysqli_num_rows($validar_login) > 0) {
         $_SESSION['usuario'] = $correo; // Variable que verifica la sesion
         header("location: dashboard.php");
