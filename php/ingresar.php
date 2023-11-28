db2c657a16b117409b302904db247c68cfb1ffbc
slaughtbear <ivanaguirre368@gmail.com>
2023-11-28 16:35:26 -0600

    devolvi todo como estaba alv

--- php/ingresar.php
+++ php/ingresar.php
@@ -13,7 +13,6 @@ session_start(); // Inicia sesión
 
     // Validacion de usuario
     $validar_login = mysqli_query($conexion, "SELECT * FROM users WHERE correo = '$correo' and contrasena = '$contrasena'");
-
     if ($contrasena!=$contrasena2) {
         echo '
         <script>
@@ -23,21 +22,15 @@ session_start(); // Inicia sesión
             ';
     } elseif(mysqli_num_rows($validar_login) > 0) {
         $_SESSION['usuario'] = $correo; // Variable que verifica la sesion
-        $rol = $usuario['rol']; // Obtener el rol del usuario desde la base de datos
-        if ($rol == 1) {
-            header("Location: https://www.youtube.com/watch?v=AWjCxj9D_Ko&t=32s");
-            exit;
-        } else {
-            header("Location: dashboard.php");
-            exit;
-        }
+        header("location: dashboard.php");
+        exit;
     } else {
         echo '
         <script>
-            alert("Usuario no existe o contraseña incorrecta, por favor verifique los datos introducidos");
+            alert("Usuario no existe, por favor verifique los datos introducidos");
             window.location = "ingresar.php";
         </script>
             ';
-        exit;
+            exit;
     }
-    ?>
+?>
