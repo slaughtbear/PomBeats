6398975549f3cda075dedcc15ea615440a3c2a78
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-27 23:43:57 -0600

    se hace la pruebe de las consultas de la tabla eventos

--- php/eventos.php
+++ php/eventos.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
    include 'conexion.php';
    $idEventos=$_GET["idEventos"];
 $sql = "SELECT * FROM eventos where idEvntos = '$idEventos'";
