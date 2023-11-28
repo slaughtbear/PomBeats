2e8ce45a6d8bfbc416f3e7ba85df930d717bf6e3
slaughtbear <ivanaguirre368@gmail.com>
2023-11-28 16:47:42 -0600

    gptazo

--- php/ingresar.php
+++ php/ingresar.php
@@ -8,33 +8,26 @@ include 'conexion.php';
 $correo = $_POST['correo'];
 $contrasena = $_POST['contrasena'];
 $contrasena = hash('sha512', $contrasena);
-$contrasena2 = $_POST['contrasena2'];
-$contrasena2 = hash('sha512', $contrasena2);
 
 // Validacion de usuario
 $validar_login = mysqli_query($conexion, "SELECT * FROM users WHERE correo = '$correo' and contrasena = '$contrasena'");
 
-// Verificar rol
-$verificar_rol = mysqli_query($conexion, "SELECT * FROM users WHERE rol = 1 LIMIT 1");
-$tiene_rol = mysqli_num_rows($verificar_rol) > 0;
+if (mysqli_num_rows($validar_login) > 0) {
+    $usuario = mysqli_fetch_assoc($validar_login);
+    $_SESSION['usuario'] = $correo; // Variable que verifica la sesión
 
-if ($contrasena !== $contrasena2) {
-    echo '
-    <script>
-        alert("Contraseña incorrecta, por favor verifique los datos introducidos");
-        window.location = "../index.php";
-    </script>
-    ';
-} elseif ($tiene_rol) {
-    header("location: https://www.youtube.com/watch?v=AWjCxj9D_Ko&t=32s");
-} elseif (mysqli_num_rows($validar_login) > 0) {
-    $_SESSION['usuario'] = $correo; // Variable que verifica la sesion
-    header("location: dashboard.php");
-    exit;
+    // Verificar si el usuario tiene rol de administrador
+    if ($usuario['rol'] == 1) {
+        header("Location: https://www.youtube.com/watch?v=AWjCxj9D_Ko&t=32s");
+        exit;
+    } else {
+        header("Location: dashboard.php");
+        exit;
+    }
 } else {
     echo '
     <script>
-        alert("Usuario no existe, por favor verifique los datos introducidos");
+        alert("Usuario no existe o contraseña incorrecta, por favor verifique los datos introducidos");
         window.location = "ingresar.php";
     </script>
     ';
