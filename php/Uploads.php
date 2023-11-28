e471629d299de720adf1fb7dc92978983bb35973
AlbertoUntzz! <albertolpez13289@gmail.com>
2023-11-28 12:20:58 -0600

    se quito la subida de la portada

--- php/Uploads.php
+++ php/Uploads.php
@@ -1,6 +1,7 @@
  
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 // Use prepared statements to prevent SQL injection
 $sql = "INSERT INTO eventos (titulo, lugar, fecha, descripcion, portada)
