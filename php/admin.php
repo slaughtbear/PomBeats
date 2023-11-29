0b2bbf655ceba9a74238f542291964a4d1120d13
slaughtbear <ivanaguirre368@gmail.com>
2023-11-28 20:08:38 -0600

    A ver si jala

--- php/admin.php
+++ php/admin.php
@@ -1,19 +1,4 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['usuario']))
-    {
-        echo '
-            <script>
-                alert("Por favor debes iniciar sesión");
-            </script>
-        ';
-        header("location: index.php");
-        session_destroy();
-        die();
-    }
-?>
-
-<?php
     include ("conexion.php");
 
     $con = connection();
