e8d81afc2f537d1ebfe53596d778cfb7788e8898
slaughtbear <ivanaguirre368@gmail.com>
2023-11-30 07:07:33 -0600

    Correccion

--- php/admin.php
+++ php/admin.php
@@ -1,17 +1,4 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['usuario']))
-    {
-        echo '
-            <script>
-                alert("Por favor debes iniciar sesión");
-            </script>
-        ';
-        //header("location: index.php");
-        session_destroy();
-        die();
-    }
-
     include ("conexion.php");
 
     $conexion = connection();
